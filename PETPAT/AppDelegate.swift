85042ce6ed7b7d233cc9366c814831c6460f4789
thepangmonster <brandonpangilinan@Brandons-Mac.local>
2017-03-03 09:47:14 -0800

    FIRApp.configure()
    A line that I forgot to type to make all the API calls work

--- PETPAT/AppDelegate.swift
+++ PETPAT/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FIRApp.configure()
         return true
     }
 
@@ -45,6 +46,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Saves changes in the application's managed object context before the application terminates.
         self.saveContext()
     }
+    
 
     // MARK: - Core Data stack
 
